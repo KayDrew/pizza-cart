bdddbd50c6930dbf74d53b89f63d557d524c16cb
KayDrew <kdtshehla@gmail.com>
2023-07-11 22:56:38 +0200

    added pay button

--- js/index.js
+++ js/index.js
@@ -2,59 +2,91 @@ function Order(){
 
 return{
 	
-	cartTotal:0,
+cartTotal:0,
 large:0,
-
+roundedLarge:0,
+paymentAmount:0,
+total:0,
+total2:0,
+error:"",
 incrementLarge(){
-this.large++;
+	
+this.large+=98.99;
 this.cartTotal+=98.99;
-
+this.total++;
+this.total2=this.cartTotal.toFixed(2);
+this.roundedLarge=this.large.toFixed(2);
 },
 
 decrementLarge(){
 	
-	
 	if(this.large>0){
-this.large--;
+		
+this.large-=98.99;
 this.cartTotal-=98.99;
+this.total--;
+this.total2=this.cartTotal.toFixed(2);
+this.roundedLarge=this.large.toFixed(2);
 
 }
 },
 
 	
 medium:0,
+roundedMedium:0,
 incrementMedium(){
-this.medium++;
+this.medium+=78.99;
 this.cartTotal+=78.99;
+this.total++;
+this.total2=this.cartTotal.toFixed(2);
+this.roundedMedium=this.medium.toFixed(2);
 },
 
 decrementMedium(){
 	
 	if(this.medium>0){
-this.medium--;
+this.medium-=78.99;
 this.cartTotal-=78.99;
+this.total--;
+this.total2=this.cartTotal.toFixed(2);
+this.roundedMedium=this.medium.toFixed(2);
 }
 },
 
 
 small:0,
+roundedSmall:0,
 incrementSmall(){
-this.small++;
+this.small+=68.99;
 this.cartTotal+=68.99;
+this.total++;
+this.total2=this.cartTotal.toFixed(2);
+this.roundedSmall=this.small.toFixed(2);
 },
 
 decrementSmall(){
 
 	if(this.small>0){
-this.small--;
+this.small-=68.99;
 this.cartTotal-=68.99;
+this.total--;
+this.total2=this.cartTotal.toFixed(2);
+this.roundedSmall=this.small.toFixed(2);
 }
 },
 
 
-addToCart(){
+pay(){
 
-this.cartTotal++;
+
+if(this.paymentAmount<this.total2){
+
+this.error="The amount is insufficient";
+
+
+}
+
+
 },
 
 
